8aeff84a203b7fec4c6a3b118feade32316e28d2
Sayandip Halder <sayandiph4@gmail.com>
2021-11-29 12:48:20 +0530

    create about section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome
+      </SectionTitle>
+      <SectionText>
+        About
+      </SectionText>
+      <Button onClick={() => {window.location='https://google.com'}} >Learn more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
